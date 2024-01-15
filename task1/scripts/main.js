36fb161ae1043d4044bb32040be29fd552bfd10e
Hackerdodo <144474666+Hackerdodo@users.noreply.github.com>
2024-01-15 20:02:07 +0300

    Add files via upload

---
+++ task1/scripts/main.js
@@ -0,0 +1,38 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "/besiktas1.png") {
+    myImage.setAttribute("src", "/besiktas2.png");
+  } else {
+    myImage.setAttribute("src", "/besiktas1.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+myButton.onclick = () => {
+    setUserName();
+  };
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    if (!localStorage.getItem("name")) {
+        setUserName();
+      } else {
+        const storedName = localStorage.getItem("name");
+        myHeading.textContent = `Mozilla is cool, ${storedName}`;
+      }
+      
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
+    
+
+    
+
+  
+  
